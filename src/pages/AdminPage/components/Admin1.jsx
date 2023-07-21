8fa6a141e12e1aec71b233e2ae7edd30125c2792
LeeTaeHeon <forever296@naver.com>
2023-07-21 23:31:58 +0900

    rename: 프로젝트 트리 설정 - pages 폴더 관리 - common 폴더 관리

---
+++ src/pages/AdminPage/components/Admin1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Admin1 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Admin1;
---
+++ src/pages/MainPage/components/Main1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main1 = () => {
+    return (
+        <div>
+            메인 1
+        </div>
+    );
+};
+
+export default Main1;
