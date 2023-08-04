c19e7bfd54e56cffcfb7f3a33a6f890fa5405e6c
LeeTaeHeon <forever296@naver.com>
2023-08-05 03:05:26 +0900

    :sparkles: Feat: 전문가 신청 페이지 추가 (미완)
    - 이미지 미리보기 미완 - 제출 버튼 클릭 및 핸들러 함수 구현 X 디자인과 약간의 기능만 완료

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AdminPage from "./pages/AdminPage/AdminPage";
 import MyPage from "./pages/MyPage/MyPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import Callback from "./pages/LoginPage/components/KakaoCallback";
+import CertificationPage from './pages/CertificationPage/CertificationPage';
 
 function App() {  
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/my" element={<MyPage />} />
         <Route path="/callback" element={<Callback />} />
+        <Route path="/certificate" element={<CertificationPage />} />
       </Routes>
     </div>
   );
