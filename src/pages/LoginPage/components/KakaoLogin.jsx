507679c7208725598a0f031e3545386a2f2772ac
제동균 <gyuni@jedong-gyun-ui-MacBookAir.local>
2023-08-16 20:52:58 +0900

    feat : accessToken 받아오기 KakaoCallback 에서 토큰 받아와서 로컬에 저장하기

--- src/pages/LoginPage/components/KakaoLogin.jsx
+++ src/pages/LoginPage/components/KakaoLogin.jsx
@@ -5,33 +5,8 @@ import "../components/styles/KakaoLogin.css";
 const KakaoLogin = () => {
   const KakaoAuothUrl = "http://49.50.163.215/api/login/kakao";
 
-  const handleLogin = async () => {
+  const handleLogin = () => {
     window.location.href = KakaoAuothUrl;
-    console.log("로그인 완료");
-    try {
-      const token = await extractTokenFromURL();
-      if (token) {
-        console.log("오류1");
-        localStorage.setItem("accessToken", token);
-      }
-    } catch (error) {
-      console.log("오류2");
-      console.error("오류발생:", error);
-    }
-  };
-
-  const extractTokenFromURL = async () => {
-    return new Promise((resolve, reject) => {
-      const token = window.location.search.split("=")[1];
-      console.log(token);
-      if (token) {
-        resolve(token);
-        console.log("오류4");
-      } else {
-        console.log("오류5");
-        reject(new Error("Token not found in URL"));
-      }
-    });
   };
 
   return (
